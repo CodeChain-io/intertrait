232c814e376be6c0f9622fc7a498e8b5018e21ae
DoHyung Kim <dohyung.kim@seasonfive.co>
2020-04-17 15:15:15 +0900

    Use the linkme shorthand for function elements
    It is a new feature available in linkme 0.2.1.
    So the linkme version requirement is bumped up to 0.2.1.

--- macros/src/gen_caster.rs
+++ macros/src/gen_caster.rs
@@ -10,15 +10,11 @@ use quote::quote;
 use quote::ToTokens;
 
 pub fn generate_caster(ty: &impl ToTokens, trait_: &Path) -> TokenStream {
-    let mut static_buf = [0u8; STATIC_BUF_LEN];
-    let static_var = format_ident!("{}", generate_static(&mut static_buf));
     let mut fn_buf = [0u8; FN_BUF_LEN];
     let fn_ident = format_ident!("{}", generate_fn(&mut fn_buf));
 
     quote! {
         #[linkme::distributed_slice(intertrait::CASTERS)]
-        static #static_var: fn() -> (std::any::TypeId, intertrait::BoxedCaster) = #fn_ident;
-
         fn #fn_ident() -> (std::any::TypeId, intertrait::BoxedCaster) {
             let type_id = std::any::TypeId::of::<#ty>();
             let caster = Box::new(intertrait::Caster::<dyn #trait_> {
@@ -31,19 +27,8 @@ pub fn generate_caster(ty: &impl ToTokens, trait_: &Path) -> TokenStream {
     }
 }
 
-const STATIC_PREFIX: &[u8] = b"__";
-const STATIC_BUF_LEN: usize = STATIC_PREFIX.len() + Simple::LENGTH;
-
 const FN_PREFIX: &[u8] = b"__";
 const FN_BUF_LEN: usize = FN_PREFIX.len() + Simple::LENGTH;
-
-fn generate_static(buf: &mut [u8]) -> &str {
-    buf[..STATIC_PREFIX.len()].copy_from_slice(STATIC_PREFIX);
-    Uuid::new_v4()
-        .to_simple()
-        .encode_upper(&mut buf[STATIC_PREFIX.len()..]);
-    unsafe { from_utf8_unchecked(&buf[..STATIC_BUF_LEN]) }
-}
 
 fn generate_fn(buf: &mut [u8]) -> &str {
     buf[..FN_PREFIX.len()].copy_from_slice(FN_PREFIX);
